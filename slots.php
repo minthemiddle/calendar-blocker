f1d2bc4cb2bccbb043b665672c06fb1998e198ce
Martin Betz <martin.betz@visablelabs.com>
2023-01-30 14:28:52 +0100

    Fix: Respect breaks

--- slots.php
+++ slots.php
@@ -22,19 +22,25 @@ $day_end = 5;
 $day_current = 1;
 $slots = [];
 
-while($currentTime <= $end && $day_current < 6) {
+while($currentTime <= $end && $day_current <= $day_end) {
     $slotEnd = $currentTime->copy()->addMinutes($slotDuration);
-    $slot = [
-        'day' => $day_current,
-        'start' => $currentTime->format('H:i'),
-        'end' => $slotEnd->format('H:i'),
-    ];
-    $slots[] = $slot;
-    $currentTime = $slotEnd->addMinutes($breakDuration);
+    if ($currentTime->hour * 100 + $currentTime->minute >= $working_hours['break_start'] &&
+        $slotEnd->hour * 100 + $slotEnd->minute <= $working_hours['break_end']) {
+        $currentTime = Carbon::createFromFormat('Hi', $working_hours['break_end'])->addMinutes($breakDuration);
+    } else {
+        $slot = [
+            'day' => $day_current,
+            'start' => $currentTime->format('H:i'),
+            'end' => $slotEnd->format('H:i'),
+        ];
+        
+        $slots[] = $slot;
+        $currentTime = $slotEnd->addMinutes($breakDuration);
 
-    if($currentTime->hour * 100 + $currentTime->minute >= $working_hours['day_end']) {
-        $day_current++;
-        $currentTime = $start;
+        if ($currentTime->hour * 100 + $currentTime->minute >= $working_hours['day_end']) {
+            $day_current++;
+            $currentTime = $start;
+        }
     }
 }
 
