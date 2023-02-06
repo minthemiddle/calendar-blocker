a738f6d334cf4de515fa109b72ab8a5460f55628
Martin Betz <martin.betz@visablelabs.com>
2023-02-06 17:45:05 +0100

    Cut: Extract into config

--- slots.php
+++ slots.php
@@ -1,25 +1,10 @@
 <?php
 require 'vendor/autoload.php';
+require 'config.php';
 
 use Carbon\Carbon;
 
-$working_hours = [
-    'day_start' => 1000,
-    'break_start' => 1230,
-    'break_end' => 1330,
-    'day_end' => 1730,
-];
 
-$slotDuration = 25;
-$breakDuration = 5;
-
-$start = Carbon::createFromFormat('Hi', $working_hours['day_start']);
-$end = Carbon::createFromFormat('Hi', $working_hours['day_end']);
-
-$currentTime = $start;
-$day_start = 1;
-$day_end = 5;
-$day_current = 1;
 $slots = [];
 
 while($currentTime <= $end && $day_current <= $day_end) {
