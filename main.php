05f4cae73810fbdce2205b972831dd677695eed2
Martin Betz <martin.betz@visablelabs.com>
2023-02-06 17:48:34 +0100

    Add: Iterate over generated events

--- main.php
+++ main.php
@@ -4,7 +4,18 @@ require 'vendor/autoload.php';
 use Carbon\Carbon;
 use Spatie\IcalendarGenerator\Components\Event;
 use Spatie\IcalendarGenerator\Components\Calendar;
+use Spatie\IcalendarGenerator\Enums\Classification;
 
+require 'slots.php';
+
+foreach ($slots as $slot) {
+    $events[] = Event::create()
+        ->name('🚧 Blocker: ')
+        ->description('ID: ' . $slot['title'])
+        ->startsAt(Carbon::today()->addDays($slot['day'])->setTime($slot['start_hour'], $slot['start_minutes']))
+        ->endsAt(Carbon::today()->addDays($slot['day'])->setTime($slot['end_hour'], $slot['end_minutes']))
+        ->classification(Classification::private());
+}
 
 // Create iCalendar data
 $calendar = Calendar::create('Blocker')
@@ -12,4 +23,5 @@ $calendar = Calendar::create('Blocker')
     ->get();
 
 // Output the iCalendar data to a file
-file_put_contents('events.ics', $calendar);
+file_put_contents('out/events.ics', $calendar);
+
