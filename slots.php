7523904c23f996458dbc51a1dd2881fd4afa01ad
Martin Betz <martin.betz@visablelabs.com>
2023-02-06 17:46:21 +0100

    Add: Random titles

--- slots.php
+++ slots.php
@@ -3,9 +3,13 @@ require 'vendor/autoload.php';
 require 'config.php';
 
 use Carbon\Carbon;
+use Chypriote\UniqueNames\Generator;
 
+$generator = new Generator();
 
 $slots = [];
+// to save a list of available slots' titles
+$titles = (array) [];
 
 while($currentTime <= $end && $day_current <= $day_end) {
     $slotEnd = $currentTime->copy()->addMinutes($slotDuration);
@@ -13,13 +17,17 @@ while($currentTime <= $end && $day_current <= $day_end) {
         $slotEnd->hour * 100 + $slotEnd->minute <= $working_hours['break_end']) {
         $currentTime = Carbon::createFromFormat('Hi', $working_hours['break_end'])->addMinutes($breakDuration);
     } else {
+        $title = $generator->generate();
+
         $slot = [
             'day' => $day_current,
             'start' => $currentTime->format('H:i'),
             'end' => $slotEnd->format('H:i'),
+            'title' => $title,
         ];
         
         $slots[] = $slot;
+        $titles[] = $title;
         $currentTime = $slotEnd->addMinutes($breakDuration);
 
         if ($currentTime->hour * 100 + $currentTime->minute >= $working_hours['day_end']) {
