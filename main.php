e00eba6eec235b22b2ae673433bb0638f0a7aaa1
Martin Betz <martinmichaelbetz@gmail.com>
2025-01-05 17:05:35 +0100

    feat: Generate events with priorities and shuffle slots

--- main.php
+++ main.php
@@ -8,13 +8,33 @@ use Spatie\IcalendarGenerator\Enums\Classification;
 
 require 'slots.php';
 
+function generate_slots_priorities($priorities, $total_slots) {
+    $slots_priorities = [];
+
+    foreach ($priorities as $priority) {
+        $count = round($priority['weight'] / 100 * $total_slots);
+        for ($i = 0; $i < $count; $i++) {
+            $slots_priorities[] = $priority['title'];
+        }
+    }
+    return $slots_priorities;
+}
+
+$slots_priorities_and_titles = generate_slots_priorities($priorities, count($slots));
+shuffle($slots_priorities_and_titles);
+
 foreach ($slots as $slot) {
-    $events[] = Event::create()
-        ->name('🚧 Blocker: ')
-        ->description('ID: ' . $slot['title'])
-        ->startsAt(Carbon::today()->addDays($slot['day'])->setTime($slot['start_hour'], $slot['start_minutes']))
-        ->endsAt(Carbon::today()->addDays($slot['day'])->setTime($slot['end_hour'], $slot['end_minutes']))
-        ->classification(Classification::private());
+    for ($day = $offset_days; $day < $days_to_generate; $day++) {
+        $priority = array_shift($slots_priorities_and_titles);
+        $startDateTime = Carbon::today()->addDays($day)->setTime($slot['start_hour'], $slot['start_minutes']);
+        $endDateTime = Carbon::today()->addDays($day)->setTime($slot['end_hour'], $slot['end_minutes']);
+        $events[] = Event::create()
+            ->name('🚧 ' . $priority)
+            ->description('Priority: ' . $priority . ', ID: ' . $slot['title'])
+            ->startsAt($startDateTime)
+            ->endsAt($endDateTime)
+            ->classification(Classification::private());
+    }
 }
 
 // Create iCalendar data
@@ -24,14 +44,3 @@ $calendar = Calendar::create('Blocker')
 
 // Output the iCalendar data to a file
 file_put_contents('out/events.ics', $calendar);
-
-// Create the CSV file
-
-$fp = fopen('out/event_titles.csv', 'w');
-
-foreach ($titles as $title) {
-    fwrite($fp, $title);
-    fwrite($fp, PHP_EOL);
-}
-
-fclose($fp);
